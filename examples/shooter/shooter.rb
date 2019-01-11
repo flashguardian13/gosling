06239a75d61b1a9baf22ca3c96fefdeb3aa78a04
Ben "Flash" Amos <flashguardian13@gmail.com>
2019-01-11 15:46:08 -0800

    TEMP: enable profiling

--- examples/shooter/shooter.rb
+++ examples/shooter/shooter.rb
@@ -1,5 +1,8 @@
 require_relative 'lib/game_window.rb'
 
+require 'ruby-prof'
+require 'time'
+
 # A simple game to showcase and test Gosling. Shoot enemy ships! Dodge bullets and indestructible asteroids!
 #
 # Controls:
@@ -9,5 +12,18 @@ require_relative 'lib/game_window.rb'
 
 if __FILE__ == $PROGRAM_NAME
   window = GameWindow.new(1024, 768, false)
+
+  profile = RubyProf::Profile.new
+  profile.exclude_common_methods!
+
+  profile.start
   window.show
+  result = profile.stop
+
+  printer = RubyProf::GraphHtmlPrinter.new(result)
+  commit = ''
+  timestamp = Time.now.strftime("%F-%T").gsub(/\D/, '-')
+  File.open(File.join(File.dirname(__FILE__), "profile-graphs-#{commit}-#{timestamp}.html"), 'w') do |f|
+    printer.print(f, :min_percent => 1)
+  end
 end
